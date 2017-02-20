384dbacea56ad8fd22f33f9366dd78d4568bdacb
Ian Bibby <ian.bibby@gmail.com>
2017-02-19 22:44:25 -0500

    Adding backtracking challenge

---
+++ pressing_buttons/README.md
@@ -0,0 +1,23 @@
+Given a string of digits, return all of the possible letter combinations that the number 
+could represent. The mapping of digits to letters is the same as you would find on a 
+telephone's buttons, as in the example below:
+
+The resulting array should be sorted lexicographically.
+
+Example
+
+For buttons = "42", the output should be
+pressingButtons(buttons) = ["ga", "gb", "gc", "ha", "hb", "hc", "ia", "ib", "ic"].
+
+Input/Output
+
+[time limit] 4000ms (rb)
+[input] string buttons
+
+A string composed of digits ranging from '2' to '9'.
+
+Constraints:
+
+0 ≤ buttons.length ≤ 7.
+
+[output] array.string
---
+++ pressing_buttons/pressing_buttons.rb
@@ -0,0 +1,30 @@
+def pressing_buttons(buttons)
+  return [] if buttons.empty?
+
+  map = {
+    2 => "abc", 3 => "def", 4 => "ghi",
+    5 => "jkl", 6 => "mno", 7 => "pqrs",
+    8 => "tuv", 9 => "wxyz"
+  }
+
+  combinations = []
+  letters = buttons.each_char.map { |b| map[b.to_i] }
+  find_combinations(letters, "", 0, letters.size, combinations)
+
+  combinations.sort!
+end
+
+def find_combinations(letters, combination, l, r, combinations)
+  if l == r
+    combinations << combination.clone
+  else
+    letters[l].each_char do |c|
+      combination << c
+      find_combinations(letters, combination, l+1, r, combinations)
+      combination.slice!(-1)  # backtrack
+    end
+  end
+end
+
+puts pressing_buttons("42") == %w{ga gb gc ha hb hc ia ib ic } ? "Passed" : "Failed"
+
